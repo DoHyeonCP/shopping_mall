227da2e2d052f08364c82c97bebb075c4ab9a3dd
Lee <ehgus8621@gmail.com>
2023-10-22 03:10:05 +0900

    jpa 테스트, querydsl 추가

--- mall/src/main/java/com/example/mall/entity/Item.java
+++ mall/src/main/java/com/example/mall/entity/Item.java
@@ -2,18 +2,17 @@ package com.example.mall.entity;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.Table;
-
 import com.example.mall.constant.ItemSellStatus;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
